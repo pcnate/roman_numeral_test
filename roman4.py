dc2b05708f786d4318b8647892ddeb112ce5050f
Nathan Baker <pcnate@gmail.com>
2015-04-05 00:35:32 +0000

    added last uncommited work, don't remember what it was at this point

---
+++ roman4.py
@@ -0,0 +1,42 @@
+# roman4.py
+
+roman_numeral_map = (
+        ('M', 1000),
+        ('CM', 900),
+        ('D',  500),
+        ('CD', 400),
+        ('C',  100),
+        ('XC',  90),
+        ('L',   50),
+        ('XL',  40),
+        ('X',   10),
+        ('IX',   9),
+        ('V',    5),
+        ('IV',   4),
+        ('I',    1))
+
+def to_roman(n):
+    '''convert integer to Roman numeral'''
+    if n > 3999:
+        raise OutOfRangeError('number out of range (must be less than 4000)')
+
+    if n <= 0:
+        raise OutOfRangeError('number out of range (must be greater than 0)')
+
+    
+
+    result = ''
+    for numeral, integer in roman_numeral_map:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    return result
+
+class OutOfMemoryError(ValueError):
+	pass
+
+class OutOfRangeError(ValueError):
+	pass
+
+class NotIntegerValue(ValueError):
+	pass
